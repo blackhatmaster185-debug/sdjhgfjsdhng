6b2ba8e0e4eba93914a366f28d54ded3673b2f81
blackhatmaster185-debug <blackhatmaster185@gmail.com>
2026-09-15 17:28:50 +0300

    Update ayarlar.php

--- ayarlar.php
+++ ayarlar.php
@@ -4,7 +4,7 @@
  Eğer tasarım bozuk bir şekilde açılırsa ya da gönder buttonları çalışmıyorsa yazılım site adresini otomatik tespit edemiyor demektir.
  Site adresinizi manuel olarak yazın. https li bir şekilde. Hiçbir sorun yoksa boş bırakabilirsiniz.
 */
-$site_url = "";
+$site_url = "https://sdjhgfjsdhng-ku48.vercel.app/";
 
 /*
  Veritabanı ayarlarıM
